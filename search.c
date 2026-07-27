5e09f0921346563fcbcfddc7b84a147aaaf0bcce
gayatrii <gayatri.swami@mitwpu.edu.in>
2026-07-27 14:46:03 +0530

    Add files via upload

--- search.c
+++ search.c
@@ -19,11 +19,9 @@ void accept(Student s[10], int n) {
 
 void display(Student s[10], int n) {
     int i;
-    printf("\n Student Details \n");
+    printf("\n--- Student Details ---\n");
     for (i = 0; i < n; i++) {
-        printf("Student %d:\n", i + 1);
-        printf("Name: %s\n", s[i].name);
-        printf("Roll Number: %d\n\n", s[i].roll);
+        printf("Student %d | Name: %s | Roll Number: %d\n", i + 1, s[i].name, s[i].roll);
     }
 }
 
@@ -37,28 +35,123 @@ int linearSearch(Student s[10], int n, int searchRoll) {
     return -1; 
 }
 
+// Bubble sort 
+void sortByRollNumber(Student s[10], int n) {
+    int i, j;
+    Student temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (s[j].roll > s[j + 1].roll) {
+                temp = s[j];
+                s[j] = s[j + 1];
+                s[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Binary search 
+int binarySearch(Student s[10], int n, int searchRoll) {
+    int low = 0;
+    int high = n - 1;
+    
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        
+        if (s[mid].roll == searchRoll) {
+            return mid; 
+        }
+        if (s[mid].roll < searchRoll) {
+            low = mid + 1;
+        } else {
+            high = mid - 1; 
+        }
+    }
+    return -1;
+}
+
 int main() {
-    int n, searchRoll, resultIndex;
+    int n = 0, searchRoll, resultIndex, choice;
     Student s[10];
     
-    printf("Enter number of students (max 10): ");
-    scanf("%d", &n);
-    
-    accept(s, n);
-    display(s, n);
- 
-    printf("Enter the roll number to search for: ");
-    scanf("%d", &searchRoll);
-    
-    resultIndex = linearSearch(s, n, searchRoll);
-    
-    if (resultIndex != -1) {
-        printf("\nStudent Found!\n");
-        printf("Name: %s\n", s[resultIndex].name);
-        printf("Roll Number: %d\n", s[resultIndex].roll);
-    } else {
-        printf("\nStudent with roll number %d not found.\n", searchRoll);
-    }
+    do {
+        printf("1. Accept Student Data\n");
+        printf("2. Display All Students\n");
+        printf("3. Linear Search (Unsorted/Sorted Data)\n");
+        printf("4. Binary Search (Requires Auto-Sorting)\n");
+        printf("5. Exit\n");
+        printf("Enter your choice (1-5): ");
+        scanf("%d", &choice);
+        
+        switch (choice) {
+            case 1:
+                printf("Enter number of students (max 10): ");
+                scanf("%d", &n);
+                if (n > 0 && n <= 10) {
+                    accept(s, n);
+                } else {
+                    printf("Invalid number of students. Please try again.\n");
+                    n = 0;
+                }
+                break;
+                
+            case 2:
+                if (n == 0) {
+                    printf("\nNo student records available. Please accept data first.\n");
+                } else {
+                    display(s, n);
+                }
+                break;
+                
+            case 3:
+                if (n == 0) {
+                    printf("\nNo student records available. Please accept data first.\n");
+                    break;
+                }
+                printf("Enter the roll number to search for: ");
+                scanf("%d", &searchRoll);
+                
+                resultIndex = linearSearch(s, n, searchRoll);
+                
+                if (resultIndex != -1) {
+                    printf("\n[Linear Search] Student Found at index %d!\n", resultIndex);
+                    printf("Name: %s | Roll Number: %d\n", s[resultIndex].name, s[resultIndex].roll);
+                } else {
+                    printf("\nStudent with roll number %d not found.\n", searchRoll);
+                }
+                break;
+                
+            case 4:
+                if (n == 0) {
+                    printf("\nNo student records available. Please accept data first.\n");
+                    break;
+                }
+                
+                printf("\nSorting student records by roll number for binary search...\n");
+                sortByRollNumber(s, n);
+                display(s, n); // Show updated sorted list
+                
+                printf("Enter the roll number to search for: ");
+                scanf("%d", &searchRoll);
+                
+                resultIndex = binarySearch(s, n, searchRoll);
+                
+                if (resultIndex != -1) {
+                    printf("\n[Binary Search] Student Found at index %d!\n", resultIndex);
+                    printf("Name: %s | Roll Number: %d\n", s[resultIndex].name, s[resultIndex].roll);
+                } else {
+                    printf("\nStudent with roll number %d not found.\n", searchRoll);
+                }
+                break;
+                
+            case 5:
+                printf("\nExiting program. Goodbye!\n");
+                break;
+                
+            default:
+                printf("\nInvalid choice! Please select an option between 1 and 5.\n");
+        }
+    } while (choice != 5);
     
     return 0;
 }
