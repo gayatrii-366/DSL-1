3edc50115e9a09ada2f89b4cd0bf2464a2dc24b3
gayatrii <gayatri.swami@mitwpu.edu.in>
2026-09-21 14:17:34 +0530

    Add files via upload

---
+++ infixtopostfix.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 50
+
+char stack[MAX];
+int top = -1;
+
+void push(char c) {
+    stack[++top] = c;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+char peek() {
+    return stack[top];
+}
+
+int isOperand(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+}
+
+int precedence(char c) {
+    if (c == '^')
+        return 3;
+    if (c == '*' || c == '/')
+        return 2;
+    if (c == '+' || c == '-')
+        return 1;
+    return 0;
+}
+
+void infixToPostfix(char infix[], char postfix[]) {
+    int i = 0, j = 0;
+    char c;
+
+    while ((c = infix[i++]) != '\0') {
+        if (isOperand(c)) {
+            postfix[j++] = c;
+        } else if (c == '(') {
+            push(c);
+        } else if (c == ')') {
+            while (top != -1 && peek() != '(') {
+                postfix[j++] = pop();
+            }
+            if (top != -1) {
+                pop();
+            }
+        } else {
+            while (top != -1 && precedence(peek()) >= precedence(c)) {
+                postfix[j++] = pop();
+            }
+            push(c);
+        }
+    }
+
+    while (top != -1) {
+        postfix[j++] = pop();
+    }
+    postfix[j] = '\0';
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+
+    printf("Enter infix expression: ");
+    scanf("%s", infix);
+
+    infixToPostfix(infix, postfix);
+    printf("Postfix expression: %s\n", postfix);
+
+    return 0;
+}
